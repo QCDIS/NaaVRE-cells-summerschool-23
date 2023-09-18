c55e62fa784aeed55db44f07b255c604abd1ae15
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-09-18 14:45:49 +0200

    process-the-list-skoulouzis update

--- process-the-list-skoulouzis/process-the-list-skoulouzis.py
+++ process-the-list-skoulouzis/process-the-list-skoulouzis.py
@@ -5,7 +5,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--a', action='store', type=int, required='True', dest='a')
+arg_parser.add_argument('--a', action='store', type=str, required='True', dest='a')
 
 
 args = arg_parser.parse_args()
@@ -13,7 +13,8 @@ print(args)
 
 id = args.id
 
-a = args.a
+import json
+a = json.loads(args.a.replace('\'','').replace('[','["').replace(']','"]'))
 
 
 
