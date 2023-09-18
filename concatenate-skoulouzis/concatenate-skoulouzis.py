7386b6871258de6c2165ffdff9004729bb4e18cb
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-09-18 13:37:29 +0200

    concatenate-skoulouzis creation

---
+++ concatenate-skoulouzis/concatenate-skoulouzis.py
@@ -0,0 +1,25 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--b', action='store', type=str, required='True', dest='b')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+b = args.b
+
+
+
+res = ''
+for elem in b:
+    res += elem
+
+print(res)
+
