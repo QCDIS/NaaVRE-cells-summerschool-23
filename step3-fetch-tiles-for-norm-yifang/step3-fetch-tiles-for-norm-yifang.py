c31c7d703e6df0b2f663e2123b6d7b0b99744222
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-09-18 14:53:50 +0200

    step3-fetch-tiles-for-norm-yifang update

--- step3-fetch-tiles-for-norm-yifang/step3-fetch-tiles-for-norm-yifang.py
+++ step3-fetch-tiles-for-norm-yifang/step3-fetch-tiles-for-norm-yifang.py
@@ -9,8 +9,6 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--remote_path_retiled', action='store', type=str, required='True', dest='remote_path_retiled')
-
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
 arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
@@ -21,7 +19,6 @@ print(args)
 
 id = args.id
 
-remote_path_retiled = args.remote_path_retiled
 
 param_hostname = args.param_hostname
 param_login = args.param_login
@@ -33,7 +30,6 @@ conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled/')
 
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled/')
-remote_path_retiled
 
 tiles = [t.strip('/') for t in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_retiled.as_posix())
          if fnmatch.fnmatch(t, 'tile_*_*/')]
