cc734d18d79596a7eda9469a52d621cb5f27d7ae
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-09-18 15:07:25 +0200

    step3-fetch-tiles-for-norm-yifang update

--- step3-fetch-tiles-for-norm-yifang/step3-fetch-tiles-for-norm-yifang.py
+++ step3-fetch-tiles-for-norm-yifang/step3-fetch-tiles-for-norm-yifang.py
@@ -30,7 +30,7 @@ conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled/')
 
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled/')
-
+remote_path_retiled = str(conf_remote_path_retiled)
 tiles = [t.strip('/') for t in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_retiled.as_posix())
          if fnmatch.fnmatch(t, 'tile_*_*/')]
 
