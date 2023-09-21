89df99f080ee96d0dc9dc860bbebaea2e15d6a71
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-09-21 11:21:41 +0200

    process-the-list-skoulouzis update

--- process-the-list-skoulouzis/process-the-list-skoulouzis.py
+++ process-the-list-skoulouzis/process-the-list-skoulouzis.py
@@ -1,4 +1,3 @@
-import calmap
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -8,7 +7,6 @@ arg_parser.add_argument('--id', action='store', type=str, required=True, dest='i
 
 arg_parser.add_argument('--a', action='store', type=str, required='True', dest='a')
 
-arg_parser.add_argument('--param_a', action='store', type=str, required='True', dest='param_a')
 
 args = arg_parser.parse_args()
 print(args)
@@ -18,16 +16,8 @@ id = args.id
 import json
 a = json.loads(args.a.replace('\'','').replace('[','["').replace(']','"]'))
 
-param_a = args.param_a
-
-conf_a = 'not_secret'
-
-conf_a = 'not_secret'
 
 
-print(param_a)
-print(calmap)
-print(conf_a)
 for elem in a:
     res = elem + '_processed'
     print(res)
