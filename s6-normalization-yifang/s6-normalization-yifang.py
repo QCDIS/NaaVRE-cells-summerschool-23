bc38e825ca6c9bf3fc04c5656bc7cb4bfe7c545b
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-09-22 13:56:24 +0200

    s6-normalization-yifang update

--- s6-normalization-yifang/s6-normalization-yifang.py
+++ s6-normalization-yifang/s6-normalization-yifang.py
@@ -32,14 +32,14 @@ param_remote_path_root = args.param_remote_path_root
 param_username = args.param_username
 
 conf_local_tmp = pathlib.Path('/tmp')
-conf_remote_path_norm = pathlib.Path(param_remote_path_root + '/norm_'+param_username)
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled_'+param_username)
+conf_remote_path_norm = pathlib.Path(param_remote_path_root + '/norm_'+param_username)
 
 conf_local_tmp = pathlib.Path('/tmp')
-conf_remote_path_norm = pathlib.Path(param_remote_path_root + '/norm_'+param_username)
 conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled_'+param_username)
+conf_remote_path_norm = pathlib.Path(param_remote_path_root + '/norm_'+param_username)
 
 
 tiles
