ae1f50d860d585311e74c074f84217a5065ebbf6
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-09-22 13:53:58 +0200

    s5-fetch-tiles-for-norm-yifang update

--- s5-fetch-tiles-for-norm-yifang/s5-fetch-tiles-for-norm-yifang.py
+++ s5-fetch-tiles-for-norm-yifang/s5-fetch-tiles-for-norm-yifang.py
@@ -1,7 +1,7 @@
+from laserfarm.remote_utils import list_remote
 import pathlib
 from laserfarm.remote_utils import get_wdclient
 import fnmatch
-from laserfarm.remote_utils import list_remote
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -9,7 +9,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--remote_path_retiled', action='store', type=str, required='True', dest='remote_path_retiled')
+arg_parser.add_argument('--remote_path_norm', action='store', type=str, required='True', dest='remote_path_norm')
 
 arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
 arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
@@ -22,7 +22,7 @@ print(args)
 
 id = args.id
 
-remote_path_retiled = args.remote_path_retiled
+remote_path_norm = args.remote_path_norm
 
 param_hostname = args.param_hostname
 param_login = args.param_login
@@ -30,12 +30,12 @@ param_password = args.param_password
 param_remote_path_root = args.param_remote_path_root
 param_username = args.param_username
 
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled_'+param_username)
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled_'+param_username)
-remote_path_retiled
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+remote_path_norm
 
 tiles = [t.strip('/') for t in list_remote(get_wdclient(conf_wd_opts), conf_remote_path_retiled.as_posix())
          if fnmatch.fnmatch(t, 'tile_*_*/')]
