23d42202a90412842e51c5b274c33f510b11edcd
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-10-08 13:57:54 +0200

    cell1-skoulouzis creation

---
+++ cell1-skoulouzis/cell1-skoulouzis.py
@@ -0,0 +1,21 @@
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--param_a', action='store', type=str, required='True', dest='param_a')
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+param_a = args.param_a
+
+
+
+res1 = param_a + 'c'
+
