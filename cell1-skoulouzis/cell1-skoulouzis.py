b025792cb1a12072bafd74836c7c4422e9bd4e3c
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2023-10-08 14:03:12 +0200

    cell1-skoulouzis update

--- cell1-skoulouzis/cell1-skoulouzis.py
+++ cell1-skoulouzis/cell1-skoulouzis.py
@@ -5,7 +5,6 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--param_a', action='store', type=str, required='True', dest='param_a')
 
 args = arg_parser.parse_args()
 print(args)
@@ -13,9 +12,15 @@ print(args)
 id = args.id
 
 
-param_a = args.param_a
 
+conf_b = 'configuration'
 
+conf_b = 'configuration'
 
-res1 = param_a + 'c'
+res1 = conf_b + 'c'
 
+import json
+filename = "/tmp/res1_" + id + ".json"
+file_res1 = open(filename, "w")
+file_res1.write(json.dumps(res1))
+file_res1.close()
